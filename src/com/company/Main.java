4e7db21461e43e43d491e2180dab4d31062f0c98
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-08-23 14:45:09 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        System.out.println(sumDigits(123));
+        System.out.println("Sum digits = " + sumDigits(123));
     }
 
     private static int sumDigits(int number) {
